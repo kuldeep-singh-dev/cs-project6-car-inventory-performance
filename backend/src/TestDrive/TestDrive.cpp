ba4f71c6bd10f9a2e07b2bae895d6f586afd6625
Ginbot Weldeab <Gweldeab4179@conestogac.on.ca>
2026-01-27 20:46:27 -0500

    Initial setup

---
+++ backend/src/TestDrive/TestDrive.cpp
@@ -0,0 +1,54 @@
+#include "TestDrive.h"
+#include <iostream>
+
+TestDrive::TestDrive(int tId, int cId, int vId) {
+	testId = tId;
+	customerId = cId;
+	vehicleId = vId;
+}
+void TestDrive::setTestId(int tId) {
+	testId = tId;
+}
+int TestDrive::getTestId() {
+	return testId;
+}
+void TestDrive::setCustomerId(int cId) {
+	customerId = cId;
+}
+int TestDrive::getCustomerId() {
+	return customerId;
+}
+void TestDrive::setVehicleId(int vId) {
+	vehicleId = vId;
+}
+int TestDrive::getVehicleId() {
+	return vehicleId;
+}
+void TestDrive::setComment(std::string comm) {
+	comment = comm;
+}
+std::string TestDrive::getComment() {
+	return comment;
+}
+//API functions
+void TestDrive::testDriveApiTest(crow::response& res, string apiRoute) {
+	res.set_header("Content-Type", "text/css");
+	res.write("Test Drive API is working at " + apiRoute );
+	res.end();
+}
+void TestDrive::getTestDriveWithId(crow::response& res, std::string apiRoute, int testID) {
+	//Implementation for getting a test drive by ID
+	res.set_header("Content-Type", "application/json");
+	res.write("{ \"testId\": " + std::to_string(testID) + ", \"customerId\": 123, \"vehicleId\": 456, \"comment\": \"Test drive comment\" }");
+	res.end();
+}
+void TestDrive::getAllTestDrives(crow::response& res, std::string apiRoute) {
+	//Implementation for getting all test drives
+	res.set_header("Content-Type", "application/json");
+	res.write("[ { \"testId\": 1, \"customerId\": 123, \"vehicleId\": 456, \"comment\": \"First test drive\" }, { \"testId\": 2, \"customerId\": 789, \"vehicleId\": 101, \"comment\": \"Second test drive\" } ]");
+	res.end();
+}
+void TestDrive::createTestDrive(crow::response& res, std::string apiRoute, int customerID, int vehicleID, std::string comment) {
+	//Implementation for creating a new test drive
+}
+
---
+++ backend/src/TestDrive/TestDrive.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <iostream>
+#include "../Library/crow_all.h"
+using namespace std;
+using namespace crow;
+
+class TestDrive {
+	private:
+		int testId;
+		int customerId;
+		int vehicleId;
+		string comment = "";
+	public:
+		TestDrive(int tId, int cId, int vId);
+		void setTestId(int tId);
+		int getTestId();
+		void setCustomerId(int cId);
+		int getCustomerId();
+		void setVehicleId(int vId);
+		int getVehicleId();
+		void setComment(string comm);
+		string getComment();
+
+		//APi functions
+		void testDriveApiTest(response& res, string apiRoute);
+		void getTestDriveWithId(response& res, string apiRoute, int testID);
+		void getAllTestDrives(response& res, string apiRoute);
+		void createTestDrive(response& res, string apiRoute, int customerID, int vehicleID, string comment = " ");
+};
