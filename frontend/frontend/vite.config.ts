ea66f39e9ce77db2badd3cca37cc8725eb80bd1b
kuldeep-singh-dev <kuldeepinsan5894@gmail.com>
2026-02-10 14:56:27 -0500

    solved cors error for initial intigration error with backend

--- frontend/frontend/vite.config.ts
+++ frontend/frontend/vite.config.ts
@@ -4,7 +4,14 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // 0.0.0.0
-    port: 5173
+    host: true,
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://backend:3000',  
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
   }
 });
